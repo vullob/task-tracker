837197486fbaca1c8eee3ea80d0ab15c056dbc36
Brian Vullo <vullo.b@husky.neu.edu>
2019-02-28 18:46:55 -0500

    add deploy and start scripts

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4794
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mkdir -p ~/.config
+mkdir -p priv/static
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+echo "Starting app..."
+
+_build/prod/rel/task_tracker/bin/task_tracker foreground
+
