ec49f61e4cad823cc3455255c50a9bde11852fb4
Brian Vullo <vullo.b@husky.neu.edu>
2019-02-28 21:35:05 -0500

    updated task validation

--- lib/taskTracker/tasks/task.ex
+++ lib/taskTracker/tasks/task.ex
@@ -15,24 +15,33 @@ defmodule TaskTracker.Tasks.Task do
 
   @doc false
   def changeset(task, attrs) do
-    change = task
+    task
     |> cast(attrs, [:title, :completed, :description, :user_id, :minutes_spent], [:desc])
     |> validate_required([:title, :completed, :minutes_spent])
     |> foreign_key_constraint(:user_id, [message: "user not found"])
-
-    change = if (attrs["description"] && String.length(attrs["description"]) > 255) do
-      change |> add_error(:description, "description must be less than 255 characters")
-    else change end
-
-    change = if (attrs["name"] && String.length(attrs["name"]) > 255) do
-      change |> add_error(:name, "name must be less than 255 characters")
-    else change end
-
-    if(attrs["minutes_spent"] != nil && rem(elem(Integer.parse(attrs["minutes_spent"]), 0), 15) != 0) do
-      change |> add_error(:minutes_spent, "minutes spent must be in 15 minute increments")
-    else
-      change
-    end
-
+    |> validate_length(:title)
+    |> validate_length(:description)
+    |> validate_minutes_spent(:minutes_spent)
   end
+
+  def validate_length(changeset, field) do
+    validate_change(changeset, field, fn _, name ->
+    cond do
+      String.length(name) > 255 && field == :title -> [title: "input cannot be longer than 255 characters"]
+      String.length(name) > 255 && field == :description -> [description: "input cannot be longer than 255 characters"]
+      true -> []
+    end
+    end)
+  end
+
+  def validate_minutes_spent(changeset, field) do
+    validate_change(changeset, field,  fn _, mins ->
+      cond do
+        rem(mins, 15) -> [minutes_spent: "minutes spent must be in 15 minute increments"]
+        mins < 0 -> [minutes_spent: "minutes spent must be positive"]
+        true -> []
+      end
+      end)
+  end
+
 end
