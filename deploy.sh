be7f9e7fa60e523aa18316cf1e3d7c04ea911a62
Brian Vullo <vullo.b@husky.neu.edu>
2019-02-28 19:00:25 -0500

    update deploy config

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,14 @@ export PORT=4794
 export NODEBIN=`pwd`/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
 
-echo "Building..."
-
-mkdir -p ~/.config
-mkdir -p priv/static
-
 mix deps.get
 mix compile
 (cd assets && npm install)
 (cd assets && webpack --mode production)
 mix phx.digest
 
-echo "Generating release..."
-mix release
+mix ecto.migrate
 
 echo "Starting app..."
 
-_build/prod/rel/task_tracker/bin/task_tracker foreground
-
+mix phx.server
