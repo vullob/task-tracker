3941ac8c16fc0382b90553c7b4647dfe4c16d18e
Brian Vullo <vullo.b@husky.neu.edu>
2019-03-11 13:09:02 -0400

    add missing unique constraint to users

--- priv/repo/migrations/20190226180557_create_users.exs
+++ priv/repo/migrations/20190226180557_create_users.exs
@@ -7,6 +7,8 @@ defmodule TaskTracker.Repo.Migrations.CreateUsers do
      timestamps()
     end
 
+    create unique_index(:users, [:email])
+
     alter table(:tasks) do
       add :user_id, references(:users, on_delete: :delete_all), null: true, default: nil
     end
