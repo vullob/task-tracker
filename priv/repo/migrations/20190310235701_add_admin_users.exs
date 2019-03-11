68745d5de4a50810ca4801f2d5945107e4c337dc
Brian Vullo <vullo.b@husky.neu.edu>
2019-03-11 12:59:17 -0400

    migration to add managers

---
+++ priv/repo/migrations/20190310235701_add_admin_users.exs
@@ -0,0 +1,10 @@
+defmodule TaskTracker.Repo.Migrations.AddAdminUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :admin, :boolean, null: false, default: false
+      add :manager_id, references(:users, on_delete: :delete_all), null: true, default: nil
+    end
+  end
+end
